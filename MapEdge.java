1d4e22ec1d133bd69826b7b0ec13ab9e23695c07
mckownqa <mckownqa@rose-hulman.edu>
2019-01-31 13:23:16 -0500

    Added getters and setters for the xml encoders and decoders. Write still doesn't write to the file

--- MapEdge.java
+++ MapEdge.java
@@ -7,12 +7,39 @@ public class MapEdge {
 	String directions;
 	
 
+	MapEdge(){
+		this.nextNode = null;
+		this.name = null;
+		this.difficulty = 0;
+		this.directions = null;
+	}
+	
 	MapEdge(String[] parameters){
 		this.name = parameters[1];
 		this.nextNode = parameters[3];
 		this.difficulty = Double.parseDouble(parameters[4]);
 		String.join(" ", Arrays.copyOfRange(parameters, 5, parameters.length - 1));
 		
+	}
+	
+	MapEdge(String name, String nextNode, double difficulty, String directions){
+		this.name = name;
+		this.nextNode = nextNode;
+		this.difficulty = difficulty;
+		this.directions = directions;
+	}
+	
+	void setNextNode(String nextNode){
+		this.nextNode = nextNode;
+	}
+	void setName(String name){
+		this.name = name;
+	}
+	void setDifficulty(double diff){
+		this.difficulty = diff;
+	}
+	void setDirections(String direc){
+		this.directions = direc;
 	}
 	
 
--- MapNode.java
+++ MapNode.java
@@ -8,8 +8,8 @@ public class MapNode {
 	double drawY;
 	boolean hasFirstAid;
 	ArrayList<MapEdge> edges;
-	
-	MapNode(String name, double longitude, double latitude, double drawX, double drawY, boolean firstAid){
+
+	MapNode(String name, double longitude, double latitude, double drawX, double drawY, boolean firstAid) {
 		this.name = name;
 		this.longitude = longitude;
 		this.latitude = latitude;
@@ -18,25 +18,84 @@ public class MapNode {
 		this.hasFirstAid = firstAid;
 		this.edges = new ArrayList<>();
 	}
-	
-	public void addEdge(MapEdge newEdge){
-		this.edges.add(newEdge);
+
+	MapNode() {
+		this.name = null;
+		this.longitude = 0;
+		this.latitude = 0;
+		this.drawX = 0;
+		this.drawY = 0;
+		this.hasFirstAid = false;
+		this.edges = new ArrayList<>();
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setDrawX(double x) {
+		this.drawX = x;
+	}
+
+	public void setDrawY(double y) {
+		this.drawY = y;
+	}
+
+	public void setFirstAid(boolean hasFirstAid) {
+		this.hasFirstAid = hasFirstAid;
 	}
 	
-	public ArrayList<MapEdge> getEdges(){
+	public void setEdges(ArrayList<MapEdge> edges){
+		this.edges = edges;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+
+	public double getDrawX() {
+		return this.drawX;
+	}
+
+	public double getDrawY() {
+		return this.drawY;
+	}
+
+	public boolean getFirstAid() {
+		return this.hasFirstAid;
+	}
+
+	public ArrayList<MapEdge> getEdges() {
 		return this.edges;
 	}
-	
-	
-	
-	public String toString(){
+
+	public void addEdge(MapEdge newEdge) {
+		this.edges.add(newEdge);
+	}
+
+	public String toString() {
 		String returnValue = "{";
-		for(MapEdge edge : this.edges){
+		for (MapEdge edge : this.edges) {
 			returnValue += String.format("<%s => %s>\n", this.name, edge.nextNode);
 		}
 		returnValue += String.format("%b}", this.hasFirstAid);
 		return returnValue;
 	}
-	
-	
+
 }
