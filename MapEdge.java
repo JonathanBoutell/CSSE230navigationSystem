62aa3debb519a0ce077f1bbd32990c977332295b
mckownqa <mckownqa@rose-hulman.edu>
2019-01-31 13:01:11 -0500

    added constructors and fields to the nodes and edges. Commands now create nodes and edges

--- MapEdge.java
+++ MapEdge.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
 
 public class MapEdge {
+	String nextNode;
+	String name;
+	double difficulty;
+	String directions;
+	
+
+	MapEdge(String[] parameters){
+		this.name = parameters[1];
+		this.nextNode = parameters[3];
+		this.difficulty = Double.parseDouble(parameters[4]);
+		String.join(" ", Arrays.copyOfRange(parameters, 5, parameters.length - 1));
+		
+	}
+	
 
 }
--- MapNode.java
+++ MapNode.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
 
 public class MapNode {
-
+	String name;
+	double longitude;
+	double latitude;
+	double drawX;
+	double drawY;
+	boolean hasFirstAid;
+	ArrayList<MapEdge> edges;
+	
+	MapNode(String name, double longitude, double latitude, double drawX, double drawY, boolean firstAid){
+		this.name = name;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.drawX = drawX;
+		this.drawY = drawY;
+		this.hasFirstAid = firstAid;
+		this.edges = new ArrayList<>();
+	}
+	
+	public void addEdge(MapEdge newEdge){
+		this.edges.add(newEdge);
+	}
+	
+	public ArrayList<MapEdge> getEdges(){
+		return this.edges;
+	}
+	
+	
+	
+	public String toString(){
+		String returnValue = "{";
+		for(MapEdge edge : this.edges){
+			returnValue += String.format("<%s => %s>\n", this.name, edge.nextNode);
+		}
+		returnValue += String.format("%b}", this.hasFirstAid);
+		return returnValue;
+	}
+	
+	
 }
