d4e02f491e1ae2566c55e6ddd457285fedcdc27e
mckownqa <mckownqa@rose-hulman.edu>
2019-02-09 12:39:16 -0500

    Fixed the mapedge directions being null

--- MapEdge.java
+++ MapEdge.java
@@ -22,7 +22,7 @@ public class MapEdge implements Drawable{
 		this.name = parameters[1];
 		this.nextNode = parameters[3];
 		this.difficulty = Double.parseDouble(parameters[4]);
-		String.join(" ", Arrays.copyOfRange(parameters, 5, parameters.length - 1));
+		this.directions = String.join(" ", Arrays.copyOfRange(parameters, 5, parameters.length - 1));
 		findColor();
 	}
 
